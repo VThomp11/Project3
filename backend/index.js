9e2d36725369544f153c0762e117f9570728488b
ella <96279971+hiaytus@users.noreply.github.com>
2023-11-21 10:06:16 -0800

    data

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import { faker } from '@faker-js/faker';
+import { faker, id_ID } from '@faker-js/faker';
 import * as fs from 'fs';
 
 let weaponArray = [];
@@ -8,11 +8,10 @@ let animalArray = [];
 
 function createWeapon(){
 
-  let item = faker.helpers.arrayElement(['nerf gun', 'water gun', 'paintball gun', 'finger guns'])
+  let item = faker.helpers.arrayElement(['nerf gun', 'water gun', 'paintball gun', 'finger guns', 'butter knife'])
   return {
-    // _id: faker.string.uuid(),
-    // product: faker.helpers.arrayElement(['drugs', 'animals', 'weapons', 'organs']),
-    product: 'weapons',
+    id: weaponArray.length,
+    product_id: 1,
     item: faker.commerce.productAdjective() + ' ' + item,
     price: faker.commerce.price(),
     description: faker.lorem.sentences({ min: 1, max: 3 }),
@@ -24,9 +23,8 @@ function createWeapon(){
 
 function createDrug(){
   return {
-    // _id: faker.string.uuid(),
-    // product: faker.helpers.arrayElement(['drugs', 'animals', 'weapons', 'organs']),
-    product: 'drugs',
+    id: drugArray.length,
+    product_id: 2,
     item: faker.commerce.productAdjective() + faker.helpers.arrayElement([' advil', ' tylenol', ' ibuprofen', ' claritin']),
     price: faker.commerce.price(),
     description: faker.lorem.sentences({ min: 1, max: 3 }),
@@ -39,9 +37,8 @@ function createDrug(){
 function createAnimal(){
   let animalType = faker.animal.type();
   return {
-    // _id: faker.string.uuid(),
-    // product: faker.helpers.arrayElement(['drugs', 'animals', 'weapons', 'organs']), 
-    product: 'animals',
+    id: animalArray.length,
+    product_id: 3,
     item: faker.commerce.productAdjective() + ' ' + animalType,
     price: faker.commerce.price(),
     description: faker.lorem.sentences({ min: 1, max: 3 }),
@@ -54,9 +51,8 @@ function createAnimal(){
 function createOrgan(){
   let organ = faker.helpers.arrayElement(['pipe organ', 'reed organ', 'water organ', 'electric organ']);
   return {
-    // _id: faker.string.uuid(),
-    // product: faker.helpers.arrayElement(['drugs', 'animals', 'weapons', 'organs']), 
-    product: 'musical instrument',
+    id: organArray.length,
+    product_id: '4',
     item: faker.commerce.productAdjective() + ' ' + organ,
     price: faker.commerce.price(),
     description: faker.lorem.sentences({ min: 1, max: 3 }),
@@ -89,6 +85,8 @@ for (var i = 0; i < 10; i++) {
 
 function toCSV(input) {
   return input.map(item => [
+    item.id,
+    item.product_id,
     item.item,
     item.price,
     item.description,
@@ -99,28 +97,16 @@ function toCSV(input) {
   .join("\n")
 }
 
+console.log(toCSV(weaponArray))
 let weaponData = toCSV(weaponArray)
 let drugData = toCSV(drugArray)
 let animalData = toCSV(animalArray)
 let organData = toCSV(organArray)
+let data = weaponData + drugData + animalData + organData
 
-fs.writeFile("weapons.txt", weaponData, (err) => {
-  if (err) 
-  console.log(err); 
-else { 
-  console.log("File written successfully\n"); 
-}
-})
+let header = "id, product_id, item, price, description, img, location\n"
 
-fs.writeFile("drugs.txt", drugData, (err) => {
-  if (err) 
-  console.log(err); 
-else { 
-  console.log("File written successfully\n"); 
-}
-})
-
-fs.writeFile("animals.txt", animalData, (err) => {
+fs.writeFile("inventory.csv", header, (err) => {
   if (err) 
   console.log(err); 
 else { 
@@ -129,11 +115,44 @@ else {
 })
 
 
-fs.writeFile("organs.txt", organData, (err) => {
+fs.appendFile("inventory.csv", data, (err) => {
   if (err) 
   console.log(err); 
 else { 
   console.log("File written successfully\n"); 
 }
 })
+
+// fs.writeFile("weapons.txt", weaponData, (err) => {
+//   if (err) 
+//   console.log(err); 
+// else { 
+//   console.log("File written successfully\n"); 
+// }
+// })
+
+// fs.writeFile("drugs.txt", drugData, (err) => {
+//   if (err) 
+//   console.log(err); 
+// else { 
+//   console.log("File written successfully\n"); 
+// }
+// })
+
+// fs.writeFile("animals.txt", animalData, (err) => {
+//   if (err) 
+//   console.log(err); 
+// else { 
+//   console.log("File written successfully\n"); 
+// }
+// })
+
+
+// fs.writeFile("organs.txt", organData, (err) => {
+//   if (err) 
+//   console.log(err); 
+// else { 
+//   console.log("File written successfully\n"); 
+// }
+// })
 
