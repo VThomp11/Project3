b0a801647fe5566782f621a546f20f24b272d8ce
GogogoDavis <burkedavis@yahoo.com>
2023-11-29 10:14:03 -0700

    details done

--- frontend/src/Pages/itemDetailsPage.js
+++ frontend/src/Pages/itemDetailsPage.js
@@ -9,20 +9,18 @@ import { itemContext } from '../App';
 import './itemDetailsPage.css'; 
 // export default ItemDetailsPage;
 export const ItemDetailsPage = () => {
-
-  const { category, productId} = useContext(itemContext);
+  const { category, productId } = useContext(itemContext);
 
   const [price, setPrice] = useState(0);
   const [item, setItem] = useState("");
   const [desc, setDesc] = useState("");
   const [img, setImg] = useState("");
-  console.log(category)
-  console.log(productId)
+  console.log(category);
+  console.log(productId);
 
-
-  useEffect(()=> {
+  useEffect(() => {
     fetch(`http://localhost:8080/${category}/${productId}`)
-    .then((res) => res.json())
+      .then((res) => res.json())
       .then((displayData) => {
         setItem(displayData[0].item);
         setPrice(displayData[0].price);
@@ -32,18 +30,35 @@ export const ItemDetailsPage = () => {
   }, []);
 
   return (
-    <container id='detailContainer'>
-      <img src={img} width="250px" alt="desc" id="detailImage"/>
-      <body id='stuff'>
-        <ul id="attributeList">
-      <li>ITEM: {item}</li>
-      <li>PRICE: ${price}</li>
-      <li>DESCRIPTION: {desc}</li>
-      </ul>
+    <container id="detailContainer">
+      <img src={img} width="250px" alt="desc" id="detailImage" />
+      <body id="stuff">
+        <div id="attributeList">
+          <div>
+            <p id="item">ITEM: {item}</p>
+          </div>
+          <div>
+            <p id="price">PRICE: ${price}</p>
+          </div>
+          <div>
+            <p id="description">DESCRIPTION: {desc}</p>
+          </div>
+        </div>
       </body>
-      <Link to={`http://localhost:3000/${category}/${productId}/edit`}>Edit</Link>
-      <Link to={`http://localhost:3000/deleted`} onClick= {()=> {handleDelete(`http://localhost:8080/${category}/${productId}`) }} >Delete</Link>
-      <Link to={`http://localhost:3000/`}>Return</Link>
+      <span id="links">
+        <Link
+          to={`http://localhost:3000/${category}/${productId}/edit`}
+          id="editLink"
+        >Edit</Link>
+        <Link to={`http://localhost:3000/deleted`}
+          onClick={() => {
+            handleDelete(`http://localhost:8080/${category}/${productId}`);
+          }}
+          id="deleteLink">
+          Delete
+        </Link>
+        <Link to={`http://localhost:3000/`} id='returnLink'>Return</Link>
+      </span>
     </container>
   );
 };
